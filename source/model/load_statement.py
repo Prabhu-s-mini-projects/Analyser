d40623b2b64ad15f7a44abd6c0164d1547bb1bf9
Prabhukumar Sivamoorthy <35177754+PrabhukumarSivamoorthy@users.noreply.github.com>
2024-10-25 00:01:08 -0700

    feat: logger dev completed ver 0.0.1

--- source/model/load_statement.py
+++ source/model/load_statement.py
@@ -20,21 +20,20 @@ class LoadStatement:
     Attributes:
         file_path : str
         __column_mapping: dict
-        __
     Methods:
-        load_csv : loads the csv file and returns the data
-        validate_columns: Validates that required columns are present after mapping.
-        rename_columns: Renames columns using user-provided mapping.
-        clean_data: Cleans up text fields, converts date and amount fields.
-        get_dataframe: Returns the cleaned and validated DataFrame.
+        __load_csv : loads the csv file and returns the data
+        __validate_columns: Validates that required columns are present after mapping.
+        __rename_columns: Renames columns using user-provided mapping.
+        __clean_data: Cleans up text fields, converts date and amount fields.
+        get_transactions: Returns the cleaned and validated DataFrame.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self,file_path, **kwargs):
         """
         Attributes:
             file_path : str
         """
-        self.file_path: str = kwargs.get("file_path")
+        self.file_path: str = file_path
         self.__column_mapping: dict = kwargs.get("column_mapping")
         self.__transaction_table: pd.DataFrame = None
 
@@ -51,6 +50,20 @@ class LoadStatement:
             log.debug(tag=Tag.MODEL, message=f"csv_file_path = {self.file_path = }")
             log.exception(tag=Tag.MODEL,message=f"{e = }")
 
+    def __validate_columns(self)-> bool:
+        """ Validates that required columns are present after mapping"""
+        return True if self.__transaction_table else False
+
+    def __rename_columns(self)-> None:
+        """ Renames columns using user-provided mapping """
+        print(self.__rename_columns.__doc__)
+
+    def __clean_data(self)-> None:
+        """Cleans up text fields, converts date and amount fields"""
+        print(self.__clean_data.__doc__)
+
     def get_transactions(self)-> pd.DataFrame:
         """returns the final validation table that contains the transactions """
-        return self.__load_csv()
+        from_csv_file = self.__load_csv()
+        self.__transaction_table =  from_csv_file
+        return self.__transaction_table
